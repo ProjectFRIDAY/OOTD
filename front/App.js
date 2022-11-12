031cad980ed59602b0bb6a424d00ba2d217af58f
SeonHwan-Kim <rlatjsghks4647@naver.com>
2022-11-12 19:48:16 +0900

    [feat] Add Login Page

--- front/App.js
+++ front/App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import LoginPage from "./src/loginpage/LoginPage";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LoginPage />
     </View>
   );
 }
@@ -13,8 +14,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E6EBE9",
   },
 });
