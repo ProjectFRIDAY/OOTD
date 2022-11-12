b4696073294027dc18385a3b5f4977cc06803609
SeonHwan-Kim <rlatjsghks4647@naver.com>
2022-11-13 03:39:11 +0900

    [feat] LoginPage to JoinMembershipPage

--- front/App.js
+++ front/App.js
@@ -2,18 +2,8 @@ import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import LoginPage from "./src/loginpage/LoginPage";
+import Navigation from "./src/navigation";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <LoginPage />
-    </View>
-  );
+  return <Navigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#E6EBE9",
-  },
-});
