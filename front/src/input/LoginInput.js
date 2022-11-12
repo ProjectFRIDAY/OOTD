4b2e663c161cdf1b21870177c9c3252f63e20afe
SeonHwan-Kim <rlatjsghks4647@naver.com>
2022-11-12 19:45:58 +0900

    [feat] Add Login Input

---
+++ front/src/input/LoginInput.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { TextInput, StyleSheet } from "react-native";
+
+export default function InputText({
+  placeHoldText,
+  handleChange,
+  keyboardType,
+  textType,
+}) {
+  const [changeText, setChangeText] = useState("");
+  useEffect(() => {
+    handleChange(changeText);
+  });
+  return (
+    <TextInput
+      style={styles.input}
+      placeholder={placeHoldText}
+      placeholderTextColor={"#BECCC8"}
+      keyboardType={keyboardType}
+      onChangeText={(text) => setChangeText(text)}
+      secureTextEntry={textType}
+      editablex
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: "#FFFFFF",
+    width: 285,
+    height: 50,
+    borderRadius: 25,
+    padding: 10,
+    margin: 10,
+  },
+});
