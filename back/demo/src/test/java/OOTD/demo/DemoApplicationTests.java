acea5890e6458e33e7a9998b307b74464d4a188d
minsang <jleeap@ajou.ac.kr>
2022-11-14 23:01:46 +0900

    project init

---
+++ back/demo/src/test/java/OOTD/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package OOTD.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
