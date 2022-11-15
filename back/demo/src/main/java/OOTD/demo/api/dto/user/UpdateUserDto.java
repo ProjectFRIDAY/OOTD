f7e73d4c53bc30dbd6a6bc48cf9cf0eb137dc6e9
minsang <jleeap@ajou.ac.kr>
2022-11-15 20:54:50 +0900

    userservice dev

---
+++ back/demo/src/main/java/OOTD/demo/api/dto/user/UpdateUserDto.java
@@ -0,0 +1,12 @@
+package OOTD.demo.api.dto.user;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateUserDto {
+    private String userName;
+    private String userProfileImg;
+}
