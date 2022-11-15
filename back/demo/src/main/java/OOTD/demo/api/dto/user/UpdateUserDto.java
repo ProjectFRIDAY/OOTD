d0a34f5b99143dcb672b42a1bb3ad328c0dbfdad
minsang <jleeap@ajou.ac.kr>
2022-11-16 01:12:03 +0900

    userController dev

--- back/demo/src/main/java/OOTD/demo/api/dto/user/UpdateUserDto.java
+++ back/demo/src/main/java/OOTD/demo/api/dto/user/UpdateUserDto.java
@@ -1,12 +1,19 @@
 package OOTD.demo.api.dto.user;
 
 
+import OOTD.demo.domain.user.User;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class UpdateUserDto {
     private String userName;
     private String userProfileImg;
+    public UpdateUserDto(User user){
+        this.userName = user.getName();
+        this.userProfileImg = user.getProfileImg();
+    }
 }
