afab97aad12e7520ef9897418f8ee98b5c5357ff
SSUN <96679633+SeonHwan-Kim@users.noreply.github.com>
2022-11-18 03:23:48 +0900

    [fix] remove NavigationContainer/

--- front/src/navigation/StackNavigation.js
+++ front/src/navigation/StackNavigation.js
@@ -10,39 +10,37 @@ const Stack = createNativeStackNavigator();
 
 export default function StackNavigation() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={LoginPage}
-          options={{ headerShown: false }}
-        ></Stack.Screen>
-        <Stack.Screen
-          name="JoinMembershipView"
-          component={JoinMembershipView}
-          options={{
-            headerStyle: {
-              backgroundColor: "#3B5448",
-            },
-            headerTitle: (props) => <LogoTitle {...props} />,
-          }}
-        ></Stack.Screen>
-        <Stack.Screen
-          name="MainPage"
-          component={TabNavigation}
-          options={{
-            headerStyle: {
-              backgroundColor: "#3B5448",
-            },
-            headerTitle: (props) => <LogoTitle {...props} />,
-            headerBackVisible: false,
-          }}
-        ></Stack.Screen>
-        <Stack.Screen
-          name="TabMiddleBtn"
-          component={TabMiddleBtn}
-        ></Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen
+        name="Login"
+        component={LoginPage}
+        options={{ headerShown: false }}
+      ></Stack.Screen>
+      <Stack.Screen
+        name="JoinMembershipView"
+        component={JoinMembershipView}
+        options={{
+          headerStyle: {
+            backgroundColor: "#3B5448",
+          },
+          headerTitle: (props) => <LogoTitle {...props} />,
+        }}
+      ></Stack.Screen>
+      <Stack.Screen
+        name="MainPage"
+        component={TabNavigation}
+        options={{
+          headerStyle: {
+            backgroundColor: "#3B5448",
+          },
+          headerTitle: (props) => <LogoTitle {...props} />,
+          headerBackVisible: false,
+        }}
+      ></Stack.Screen>
+      <Stack.Screen
+        name="TabMiddleBtn"
+        component={TabMiddleBtn}
+      ></Stack.Screen>
+    </Stack.Navigator>
   );
 }
