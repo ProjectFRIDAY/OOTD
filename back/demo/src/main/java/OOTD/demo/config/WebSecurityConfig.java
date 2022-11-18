244871771b266cd3ae7e702825b2ec1fb042a6a5
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2022-11-18 18:14:40 +0900

    [fix] disable filtering for some urls
    1. Disable filtering for some urls in Spring Secutiry (/test/**)

--- back/demo/src/main/java/OOTD/demo/config/WebSecurityConfig.java
+++ back/demo/src/main/java/OOTD/demo/config/WebSecurityConfig.java
@@ -1,4 +1,36 @@
 package OOTD.demo.config;
 
-public class WebSecurityConfig {
+import OOTD.demo.service.user.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsUtils;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .exceptionHandling()
+                .and()
+                .authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+
+                .antMatchers("/test/**").permitAll()
+
+                .anyRequest().authenticated();
+    }
 }
