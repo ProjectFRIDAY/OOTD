21bb3ce11add64863c9098e178e00ea2647f9567
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2022-11-18 13:42:28 +0900

    [feat] add http response generator
    1. add HttpResponseUtil (To create a responseEntity in a fixed format)
    2. change DiaryController to use HttpResponseUtil

---
+++ back/demo/src/main/java/OOTD/demo/domain/HttpResponseUtil.java
@@ -0,0 +1,31 @@
+package OOTD.demo.domain;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.http.HttpStatus.OK;
+
+/**
+ * 컨트롤러에서 반환하는 공통 양식을 생성하는 Util 클래스입니다.
+ * @version 1.0.0
+ * @author CHO Min Ho
+ */
+@Component
+public class HttpResponseUtil {
+
+    /**
+     * 정상적인 요청에 대한 성공시 컨트롤러에서 반환할 객체를 생성하는 메서드입니다.
+     * @param object 실제 return되는 데이터
+     * @param responseMessage 응답 메시지
+     * @return 반환할 responseEntity 객체
+     */
+    public ResponseEntity<?> createOKHttpResponse(Object object, String responseMessage) {
+        Message message = new Message();
+        HttpHeaders headers = new HttpHeaders();
+        message.setStatus(Message.StatusEnum.OK);
+        message.setMessage(responseMessage);
+        message.setData(object);
+        return new ResponseEntity<>(message, headers, OK);
+    }
+}
