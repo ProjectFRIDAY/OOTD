236eaccbd142798e784ac1bca2fed0f4d4ce0aa1
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2022-11-18 13:48:04 +0900

    [feat] add delete diary like controller method
    1. add delete diary like controller method

--- back/demo/src/main/java/OOTD/demo/diarylike/controller/DiaryLikeController.java
+++ back/demo/src/main/java/OOTD/demo/diarylike/controller/DiaryLikeController.java
@@ -7,12 +7,12 @@ import OOTD.demo.domain.HttpResponseUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 게시글 좋아요 관련 컨트롤러 클래스입니다.
+ * @version 1.0.0
+ * @author CHO Min Ho
  */
 @RestController
 @RequiredArgsConstructor
@@ -34,6 +34,20 @@ public class DiaryLikeController {
 
         PostDiaryLikeResDTO diaryLike = diaryLikeService.createDiaryLike(dto, null);
         return httpResponseUtil.createOKHttpResponse(diaryLike, "좋아요 요청에 성공했습니다.");
+    }
+
+    /**
+     * 게시글 좋아요를 삭제하는 API입니다.
+     * @param id 삭제할 DiaryLike 엔티티의 ID
+     * @return 성공 메시지
+     */
+    @GetMapping("/api/diary/like/delete/{id}")
+    public ResponseEntity<?> deleteDiaryLike(@PathVariable(name = "id") Long id) {
+
+        // TODO : 현재 로그인된 사용자를 불러오는 로직 필요
+
+        diaryLikeService.deleteDiaryLike(id, null);
+        return httpResponseUtil.createOKHttpResponse(null, "좋아요 취소에 성공했습니다.");
     }
 
 }
