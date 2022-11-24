49f0bc1455904809c82212bd5c3688ea6b1d292a
SeonHwan-Kim <rlatjsghks4647@naver.com>
2022-11-25 01:20:21 +0900

    [feat] Add Scroll View

--- front/src/joinMembership/JoinMembershipView.js
+++ front/src/joinMembership/JoinMembershipView.js
@@ -1,28 +1,30 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, ScrollView } from "react-native";
 import Btn from "../button/Btn";
 import InfoInput from "./InfoInput";
 import UploadImageBtn from "./UploadImageBtn";
 
 export default function JoinMembershipView() {
   return (
-    <View style={styles.background}>
-      <View style={{ marginTop: 108, marginBottom: 30 }}>
-        <UploadImageBtn />
+    <ScrollView style={{ height: "100%", backgroundColor: "#E6EBE9" }}>
+      <View style={styles.background}>
+        <View style={{ marginTop: 108, marginBottom: 30 }}>
+          <UploadImageBtn />
+        </View>
+        <InfoInput />
+        <Btn text={"회원가입하기"} />
+        <Text style={styles.agreeText}>
+          인증을 완료하면 서비스 이용약관과 개인정보처리방침에 동의한 것으로
+          간주합니다
+        </Text>
       </View>
-      <InfoInput />
-      <Btn text={"인증코드 요청"} />
-      <Text style={styles.agreeText}>
-        인증을 완료하면 서비스 이용약관과 개인정보처리방침에 동의한 것으로
-        간주합니다
-      </Text>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    backgroundColor: "#E6EBE9",
+    // backgroundColor: "#E6EBE9",
     width: "100%",
     height: "100%",
     alignItems: "center",
@@ -31,5 +33,6 @@ const styles = StyleSheet.create({
     fontSize: 10,
     color: "#598471",
     margin: 10,
+    marginBottom: 30,
   },
 });
