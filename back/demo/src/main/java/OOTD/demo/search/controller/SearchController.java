87d194ecd4da219e96685d6e38d919d8b84b44c1
taewan <taewan2002@naver.com>
2022-12-29 00:16:07 +0900

    [fix] fix search Controller

--- back/demo/src/main/java/OOTD/demo/search/controller/SearchController.java
+++ back/demo/src/main/java/OOTD/demo/search/controller/SearchController.java
@@ -1,11 +1,12 @@
 package OOTD.demo.search.controller;
 
 import OOTD.demo.common.HttpResponseUtil;
+import OOTD.demo.diary.dto.DiaryDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import OOTD.demo.search.dto.*;
+import OOTD.demo.diary.service.DiaryService;
 
 /**
  * TagName, DiaryTitle, UserName을 검색하는 컨트롤러 클래스입니다.
@@ -22,29 +23,24 @@ public class SearchController {
      * @param TagName, DiaryTitle, UserName
      * @return 검색 결과
      */
+    private final DiaryService diaryService;
     private final HttpResponseUtil httpResponseUtil;
     @PostMapping("/api/search/TagName/{TagName}")
-    public ResponseEntity<?> searchTagName(@PathVariable("TageName") String TagName, @RequestBody SearchDTO dto) {
+    public ResponseEntity<?> searchTagName(@PathVariable(name = "TagName") Long id, @RequestBody DiaryDTO dto) {
 
-        // TODO : 데이터 베이스에서 TagName 검색 결과를 불러오는 로직 필요
-        // TagName 검색 결과를 불러온다. -> DiaryTitle 리스트 반환
-
+        // TODO :
         return httpResponseUtil.createOKHttpResponse(dto, "TagName 검색 결과");
     }
     @PostMapping("/api/search/DiaryTitle/{DiaryTitle}")
-    public ResponseEntity<?> searchDiaryTitle(@PathVariable("DiaryTitle") String DiaryTitle, @RequestBody SearchDTO dto) {
+    public ResponseEntity<?> searchDiaryTitle(@PathVariable(name = "DiaryTitle") String DiaryTitle, @RequestBody DiaryDTO dto) {
 
-        // TODO : 데이터 베이스에서 DiaryTiitle 검색 결과를 불러오는 로직 필요
-        // DiaryTitle 검색 결과를 불러온다. -> DiaryTitle 리스트 반환
-
+        // TODO :
         return httpResponseUtil.createOKHttpResponse(dto, "DiaryTitle 검색 결과");
     }
     @PostMapping("/api/search/UserName/{UserName}")
-    public ResponseEntity<?> searchUserName(@PathVariable("UserName") String UserName, @RequestBody SearchDTO dto) {
+    public ResponseEntity<?> searchUserName(@PathVariable(name = "UserName") String UserName, @RequestBody DiaryDTO dto) {
 
-        // TODO : 데이터 베이스에서 UserName 검색 결과를 불러오는 로직 필요
-        // UserName 검색 결과를 불러온다. -> 유저가 가진 DiaryTitle 리스트 반환
-
+        // TODO :
         return httpResponseUtil.createOKHttpResponse(dto, "UserName 검색 결과");
     }
 }
