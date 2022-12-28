976ef8e1d72d9c1c5477eeb64aa9cb296fe642d8
taewan <taewan2002@naver.com>
2022-12-29 00:15:45 +0900

    [fix] fix search Repository

--- back/demo/src/main/java/OOTD/demo/search/repository/SearchRepository.java
+++ back/demo/src/main/java/OOTD/demo/search/repository/SearchRepository.java
@@ -1,9 +1,6 @@
 package OOTD.demo.search.repository;
 
 import OOTD.demo.diary.Diary;
-import OOTD.demo.diary.dto.PostDiaryResDTO;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
@@ -13,7 +10,7 @@ import java.util.List;
  * @author CHO Tae Wan
  */
 public interface SearchRepository extends JpaRepository<Diary, Long> {
-    Page<PostDiaryResDTO> findByTagName(String tagName, Pageable pageable);
-    Page<PostDiaryResDTO> findByDiaryTitle(String diaryTitle, Pageable pageable);
-    Page<PostDiaryResDTO> findByUserName(String userName, Pageable pageable);
+    List<Diary> findByTagName(String tagName);
+    List<Diary> findByDiaryTitle(String diaryTitle);
+    List<Diary> findByUserName(String userName);
 }
