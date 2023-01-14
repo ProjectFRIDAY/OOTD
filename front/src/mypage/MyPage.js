10bb86180ba51eba83588e1b8cfbc77de018bb7a
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-01-15 00:27:11 +0900

    [add] add MyPage.js

---
+++ front/src/mypage/MyPage.js
@@ -0,0 +1,9 @@
+import { View, Text, Image } from "react-native";
+
+export default function MyPage() {
+  return (
+    <View style={{ height: "100%" }}>
+      <Text>hi</Text>
+    </View>
+  );
+}
