4472f498e7f5f383ec7e7256f2498f9c6a5bbf32
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-01-14 11:49:59 +0900

    [feat] add keyboard avoiding view

--- front/src/joinMembership/JoinMembershipView.js
+++ front/src/joinMembership/JoinMembershipView.js
@@ -1,32 +1,59 @@
-import { useState } from "react";
-import { Text, View, StyleSheet, ScrollView } from "react-native";
+import { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  Platform,
+  KeyboardAvoidingView,
+  NativeModules,
+} from "react-native";
 import Btn from "../button/Btn";
 import InfoInput from "./InfoInput";
 import UploadImageBtn from "./UploadImageBtn";
+
+const { StatusBarManager } = NativeModules;
 
 export default function JoinMembershipView({ navigation }) {
   const [checkEssentialFill, setCheckEssentialFill] = useState(false);
   const handleCheckEssentialFill = (isFill) => {
     isFill ? setCheckEssentialFill(true) : setCheckEssentialFill(false);
   };
+
+  useEffect(() => {
+    Platform.OS == "ios"
+      ? StatusBarManager.getHeight((statusBarFrameData) => {
+          setStatusBarHeight(statusBarFrameData.height);
+        })
+      : null;
+  }, []);
+
+  const [statusBarHeight, setStatusBarHeight] = useState(0);
+
   return (
-    <ScrollView style={{ height: "100%", backgroundColor: "#E6EBE9" }}>
-      <View style={styles.background}>
-        <View style={{ marginTop: 108, marginBottom: 30 }}>
-          <UploadImageBtn />
+    <KeyboardAvoidingView
+      style={styles.keyboard}
+      keyboardVerticalOffset={statusBarHeight + 44}
+      behavior={"padding"}
+    >
+      <ScrollView style={{ height: "100%", backgroundColor: "#E6EBE9" }}>
+        <View style={styles.background}>
+          <View style={{ marginTop: 108, marginBottom: 30 }}>
+            <UploadImageBtn />
+          </View>
+          <InfoInput handleCheckEssentialFill={handleCheckEssentialFill} />
+          <Btn
+            text={"회원가입하기"}
+            isFill={checkEssentialFill}
+            navigation={navigation}
+          />
+          <Text style={styles.agreeText}>
+            인증을 완료하면 서비스 이용약관과 개인정보처리방침에 동의한 것으로
+            간주합니다
+          </Text>
         </View>
-        <InfoInput handleCheckEssentialFill={handleCheckEssentialFill} />
-        <Btn
-          text={"회원가입하기"}
-          isFill={checkEssentialFill}
-          navigation={navigation}
-        />
-        <Text style={styles.agreeText}>
-          인증을 완료하면 서비스 이용약관과 개인정보처리방침에 동의한 것으로
-          간주합니다
-        </Text>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
