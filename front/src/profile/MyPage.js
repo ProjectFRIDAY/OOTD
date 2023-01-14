ac521318344685b1dd546b7e147bb33b4722e614
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-01-15 05:47:51 +0900

    [add] add mypage view

---
+++ front/src/profile/MyPage.js
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import ProfileView from "./ProfileView";
+
+export default function MyPage() {
+  return (
+    <View>
+      <ProfileView />
+    </View>
+  );
+}
