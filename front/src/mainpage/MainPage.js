17154f4a0fea39ed584efc081d4dc2e9356e1e6e
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-01-16 22:43:26 +0900

    [style] add bottom height

--- front/src/mainpage/MainPage.js
+++ front/src/mainpage/MainPage.js
@@ -4,7 +4,14 @@ import Ranking from "./Ranking";
 
 export default function MainPage({ navigation }) {
   return (
-    <View style={{ width: "100%", height: "100%", backgroundColor: "#EFF1F0" }}>
+    <View
+      style={{
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#EFF1F0",
+        paddingBottom: 90,
+      }}
+    >
       <CalendarView />
       <Ranking navigation={navigation} />
     </View>
