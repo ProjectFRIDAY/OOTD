970ebd64dc27262f29673a3f3b6bb44e27152b40
Min-Ho CHO <chominho96@naver.com>
2023-01-18 16:12:16 +0900

    [feat] edit diary service (create diary)
    After:
     - create diary without file upload feature
    Before:
     - add file upload feature

--- back/demo/src/main/java/OOTD/demo/diary/service/DiaryService.java
+++ back/demo/src/main/java/OOTD/demo/diary/service/DiaryService.java
@@ -6,12 +6,20 @@ import OOTD.demo.diary.dto.PostDiaryReqDTO;
 import OOTD.demo.diary.dto.PostDiaryResDTO;
 import OOTD.demo.diary.dto.UpdateDiaryReqDTO;
 import OOTD.demo.diary.repository.DiaryRepository;
+import OOTD.demo.diary_image.DiaryImage;
+import OOTD.demo.diary_image.repository.DiaryImageRepository;
+import OOTD.demo.file.FileUploadUtil;
 import OOTD.demo.user.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
+
+import static OOTD.demo.diary_image.DiaryImage.createDiaryImage;
 
 /**
  * Diary 엔티티 관련 서비스 클래스입니다.
@@ -25,16 +33,25 @@ import java.util.Optional;
 public class DiaryService {
 
     private final DiaryRepository diaryRepository;
+    private final FileUploadUtil fileUploadUtil;
+    private final DiaryImageRepository diaryImageRepository;
 
     /**
      * 게시글을 생성하는 메서드입니다.
-     * TODO : 파일 업로드 관련 로직 추가
+     * TODO : 날씨 관련 로직 추가
      * @param dto 게시글 생성 관련 엔티티
      * @param user 게시글 생성 User
+     * @param files 게시글 이미지 리스트
      * @return 생성된 게시글 엔티티의 ID를 포함한 DTO
      */
-    public PostDiaryResDTO createPost(PostDiaryReqDTO dto, User user) {
+    public PostDiaryResDTO createPost(PostDiaryReqDTO dto, List<MultipartFile> files, User user) {
         Diary diary = diaryRepository.save(Diary.createPost(dto, user));
+
+        for (int i = 1; i <= files.size(); i++) {
+            String diaryImageUrl = fileUploadUtil.uploadFile("diary", files.get(i - 1));
+            diaryImageRepository.save(createDiaryImage(i, diary, diaryImageUrl));
+        }
+
         return new PostDiaryResDTO(diary.getId());
     }
 
