44bdd5923955b605fe3bdf2e600ba2534fe91e8b
Min-Ho CHO <chominho96@naver.com>
2023-01-18 16:46:29 +0900

    [feat] add swagger (auth)
    1. Added swagger (auth)

--- back/demo/src/main/java/OOTD/demo/auth/controller/AuthController.java
+++ back/demo/src/main/java/OOTD/demo/auth/controller/AuthController.java
@@ -9,6 +9,7 @@ import OOTD.demo.common.Message;
 import OOTD.demo.user.User;
 import OOTD.demo.auth.service.AuthService;
 import OOTD.demo.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -27,6 +28,8 @@ public class AuthController {
     private final UserService userService;
     private final AuthService authService;
 
+    @Operation(summary = "회원가입 API", description = "회원가입 API 입니다.",
+            tags = { "Auth Controller" })
     @PostMapping("/api/auth/create")
     public ResponseEntity createUser(@RequestBody CreateUserDto user){
         Message message = new Message();
@@ -42,6 +45,8 @@ public class AuthController {
                 ResponseEntity.badRequest().build();
     }
 
+    @Operation(summary = "닉네임 중복체크 API", description = "닉네임 중복체크 API 입니다.",
+            tags = { "Auth Controller" })
     @PostMapping("/api/auth/checkname")
     public ResponseEntity<?> checkName(@RequestBody CheckNameDto name){
         Message message = new Message();
@@ -53,6 +58,8 @@ public class AuthController {
         }
         return ResponseEntity.ok().body("사용 가능한 닉네임입니다.");
     }
+    @Operation(summary = "로그인 API", description = "로그인 API 입니다.",
+            tags = { "Auth Controller" })
     @PostMapping("/api/auth/login")
     public ResponseEntity<?> login(HttpServletResponse response, @RequestBody LoginDto dto){
         User member = authService.login(dto);
@@ -65,12 +72,16 @@ public class AuthController {
         message.setData(member.getId());
         return new ResponseEntity<>(message, headers, HttpStatus.OK);
     }
+    @Operation(summary = "로그아웃 API", description = "로그아웃 API 입니다.",
+            tags = { "Auth Controller" })
     @PostMapping("/api/auth/logout")
     public ResponseEntity<?> logout(HttpServletResponse response){
         response.setHeader("Auth", null);
         return ResponseEntity.ok().body("로그아웃 성공");
     }
 
+    @Operation(summary = "비밀번호 확인 API", description = "비밀번호 확인 API 입니다.",
+            tags = { "Auth Controller" })
     @PostMapping("/api/auth/checkpassword/{user-id}")
     public ResponseEntity<?> checkPassword(@PathVariable("user-id") Long id,
                                            @RequestBody CheckPasswordDto dto){
