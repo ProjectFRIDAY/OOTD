be9b63a22af6e9bad32a453950e66b8ffb268a47
taewan <taewan2002@naver.com>
2023-02-14 11:48:02 +0900

    [create] edit search diary Repository

---
+++ back/demo/src/main/java/OOTD/demo/search/repository/SearchRepository.java
@@ -0,0 +1,15 @@
+package OOTD.demo.search.repository;
+
+import OOTD.demo.diary.Diary;
+import OOTD.demo.search.dto.SearchDTO;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SearchRepository extends JpaRepository<Diary, String> {
+
+    Optional<List<SearchDTO>> findAllByTitle(String title);
+
+    Optional<List<SearchDTO>> findAllByTag(String tag);
+}
