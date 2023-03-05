1b46dff41c3df173a689b04600187993ad2cf29b
Min-Ho CHO <chominho96@naver.com>
2023-03-05 14:58:05 +0900

    feat: Create Dress Hash Tag

---
+++ back/demo/src/main/java/OOTD/demo/dress/repository/DressHashTagRepository.java
@@ -0,0 +1,13 @@
+package OOTD.demo.dress.repository;
+
+import OOTD.demo.dress.DressHashTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * DressHashTag JPA Repository 입니다.
+ *
+ * @author CHO Min Ho
+ */
+public interface DressHashTagRepository extends JpaRepository<DressHashTag, Long> {
+
+}
