93ea9d9d32b6a93e1bcf6e0eb617872b566eabc6
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2023-03-13 22:56:16 +0900

    feat: add spring security filter

---
+++ back/demo/src/main/java/OOTD/demo/config/SecurityConfig.java
@@ -0,0 +1,84 @@
+package OOTD.demo.config;
+
+import OOTD.demo.auth.TokenProvider;
+import OOTD.demo.auth.filter.JwtAccessDeniedHandler;
+import OOTD.demo.auth.filter.JwtAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // Spring Security 활성화
+@EnableMethodSecurity // @PreAuthorize 어노테이션 메소드 단위로 추가하기 위해 적용 (default : true)
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+
+    // 비밀번호 암호화
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                // Spring Security should completely ignore URLs starting with /resources/
+                .antMatchers("/resources/**");
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                // 토큰을 사용하기 때문에 csrf 설정 disable
+                .csrf().disable()
+
+                // 예외 처리 시 직접 만들었던 클래스 추가
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                // 세션 사용하지 않기 때문에 세션 설정 STATELESS
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                // 토큰이 없는 상태에서 요청이 들어오는 API들은 permitAll
+                .and()
+                .authorizeHttpRequests()
+                .antMatchers("/test/**").permitAll()
+                .antMatchers("/api/auth/create").permitAll()
+                .antMatchers("/api/auth/login").permitAll()
+                .antMatchers("/api/auth/checkname").permitAll()
+                .antMatchers("/test/**").permitAll()
+
+                // swagger 관련 처리
+                .antMatchers("/swagger/**").permitAll()
+                .antMatchers("/swagger-ui/**").permitAll()
+                .antMatchers("/swagger-ui.html").permitAll()
+                .antMatchers("/swagger-resources/**").permitAll()
+                .antMatchers("/api-docs/**").permitAll()
+                // swagger 관련 처리
+
+                .anyRequest().authenticated()
+
+                // JwtFilter 를 addFilterBefore 로 등록했던 jwtSecurityConfig 클래스 적용
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
+
+        return http.build();
+    }
+}
