947a0c7170ec8e57a5d4b65de5d937c3bbf146f1
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2023-03-13 22:55:25 +0900

    feat: edit user entity to use jwt

--- back/demo/src/main/java/OOTD/demo/user/User.java
+++ back/demo/src/main/java/OOTD/demo/user/User.java
@@ -1,17 +1,9 @@
 package OOTD.demo.user;
 
 
-import OOTD.demo.auth.dto.CreateUserReq;
 import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * User 엔티티입니다.
@@ -23,24 +15,30 @@ import java.util.stream.Collectors;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "USER")
-public class User implements UserDetails
-{
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id;
+
     @Column(name = "user_email")
     private String email;
+
     @Column(name = "user_password")
     private String password;
+
     @Column(name="user_account_name")
     private String accountName;
+
     @Column(name = "user_nickname")
     private String nickName;
+
     @Column(name = "user_birth")
     private LocalDate birth;
+
     @Column(name = "user_profile_image")
     private String profileImg;
+
 
     private User(String email, String password, String accountName, String nickName, LocalDate birth) {
         this.email = email;
@@ -59,41 +57,6 @@ public class User implements UserDetails
         return new User(email, password, accountName, nickName, birth);
     }
 
-    @ElementCollection(fetch = FetchType.EAGER)
-    private List<String> roles = new ArrayList<>();
-
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return this.roles.stream()
-                .map(SimpleGrantedAuthority::new)
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 
     public void setAccountName(String accountName) {
         this.accountName = accountName;
