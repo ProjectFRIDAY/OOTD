a478fdb80c17540ffac2ae089df4394214826bcf
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2023-03-14 21:58:27 +0900

    setting: add scheduler annotation

--- back/demo/src/main/java/OOTD/demo/DemoApplication.java
+++ back/demo/src/main/java/OOTD/demo/DemoApplication.java
@@ -5,7 +5,9 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "OOTD API Server URL")})
 @EnableJpaAuditing
 @SpringBootApplication
