678921cc1445c382a7f69766cf7f818cca6cd4e9
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2023-03-14 21:57:16 +0900

    refactor: refactor jwt filter

--- back/demo/src/main/java/OOTD/demo/auth/filter/JwtFilter.java
+++ back/demo/src/main/java/OOTD/demo/auth/filter/JwtFilter.java
@@ -1,6 +1,7 @@
 package OOTD.demo.auth.filter;
 
 import OOTD.demo.auth.TokenProvider;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,15 +16,11 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @Slf4j
+@RequiredArgsConstructor
 public class JwtFilter extends GenericFilterBean {
 
     public static final String AUTHORIZATION_HEADER = "Authorization";
-
     private final TokenProvider tokenProvider;
-
-    public JwtFilter(TokenProvider tokenProvider){
-        this.tokenProvider = tokenProvider;
-    }
 
     // 실제 필터링 로직 작성
     // doFilter : 토큰의 인증 정보를 SecurityContext에 저장
@@ -33,29 +30,35 @@ public class JwtFilter extends GenericFilterBean {
 
         // resolveToken을 통해 토큰을 받아옴
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-        String jwt = resolveToken(httpServletRequest);
+        String accessToken = resolveAccessToken(httpServletRequest);
         String requestURI = httpServletRequest.getRequestURI();
 
         // 토큰 유효성 검증 후 정상이면 SecurityContext에 저장
-        if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
-            Authentication authentication = tokenProvider.getAuthentication(jwt);
+        if (StringUtils.hasText(accessToken) && tokenProvider.validateToken(accessToken)) {
+
+            Authentication authentication = tokenProvider.getAuthentication(accessToken);
             SecurityContextHolder.getContext().setAuthentication(authentication);
             log.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
-        } else log.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
+
+        } else {
+            log.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
+        }
 
         // 생성한 필터 실행
         chain.doFilter(httpServletRequest, response);
     }
 
     // Request Header에서 토큰 정보를 꺼내오기
-    private String resolveToken(HttpServletRequest request) {
+    private String resolveAccessToken(HttpServletRequest request) {
+
         String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
 
-            System.out.println("token : " + bearerToken);
+            log.info("token : {}", bearerToken);
 
             return bearerToken.substring(7);
         }
         return null;
     }
+
 }
